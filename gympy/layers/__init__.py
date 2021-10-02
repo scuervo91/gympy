a801943054af3de3a85b6e81a38b8229f0cfb82b
Santiago Cuervo <scuervo91@gmail.com>
2021-10-01 23:01:12 -0500

    forward rnn

--- gympy/layers/__init__.py
+++ gympy/layers/__init__.py
@@ -1,2 +1,2 @@
-from .layers import Linear, Sigmoid, Relu, Softmax, Tanh, layers_types
+from .layers import Linear, Sigmoid, Relu, Softmax, Tanh, layers_types, RNN
 from .functions import linear, relu, relu_derivative, sigmoid, sigmoid_derivative, softmax, tanh, tanh_derivative
