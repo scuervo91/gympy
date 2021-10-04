572537fde3340705b1011171fbefc84516b1df3a
Santiago Cuervo <scuervo91@gmail.com>
2021-10-03 23:04:20 -0500

    add rlayer file

--- gympy/layers/__init__.py
+++ gympy/layers/__init__.py
@@ -1,2 +1,3 @@
-from .layers import Linear, Sigmoid, Relu, Softmax, Tanh, layers_types, RNN
+from .layers import Linear, Sigmoid, Relu, Softmax, Tanh, layers_types
+from .rlayers import RNN
 from .functions import linear, relu, relu_derivative, sigmoid, sigmoid_derivative, softmax, tanh, tanh_derivative
